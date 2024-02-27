3b1600e7d98bbd3b14563d01117b3f1ace0a6224
luqs21 <luqmanhaqim21@gmail.com>
2024-02-28 00:35:45 +0800

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Test App</title>
+        
+<!-- Fonts -->
+
+        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/app.css">
+<script src="app.js"></script>
+     
+
+        
+        
+    </head>
+    <body>
+        <div class="container">
+           <article>
+            <h1><a href="/post"> First Post</a></h1>
+
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa laboriosam voluptates ratione corporis tempore repellat laudantium adipisci saepe unde nulla. Officia voluptates exercitationem nihil laudantium consequuntur similique dolorum esse quidem.
+            </p>
+           </article>
+           <article>
+            <h1><a href="/post"> Second Post</a></h1>
+
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa laboriosam voluptates ratione corporis tempore repellat laudantium adipisci saepe unde nulla. Officia voluptates exercitationem nihil laudantium consequuntur similique dolorum esse quidem.
+            </p>
+           </article>
+           <article>
+            <h1><a href="/post"> third Post</a></h1>
+
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa laboriosam voluptates ratione corporis tempore repellat laudantium adipisci saepe unde nulla. Officia voluptates exercitationem nihil laudantium consequuntur similique dolorum esse quidem.
+            </p>
+           </article>
+        </div>
+    </body>
